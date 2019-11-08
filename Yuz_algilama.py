9230196189972ec241f230280fc27aabb63d3297
Ali Vahap Aydın <ali_vahap14@hotmail.com>
2019-11-08 14:55:56 +0300

    yüz tanımlama işleminde resimleri tarayıp id ataması yaptım

--- Yuz_algilama.py
+++ Yuz_algilama.py
@@ -11,9 +11,9 @@ while True:
     gri_renk = cv2.cvtColor(kare,cv2.COLOR_BGR2GRAY)
   #  gri_renk_gulme=cv2.cvtColor(kare,cv2.COLOR_BGR2GRAY)
 
-    faces = yuz.detectMultiScale(gri_renk,1.3,4)
+    yuzler = yuz.detectMultiScale(gri_renk,1.3,4)
 
-    for(x,y,w,h) in faces:
+    for(x,y,w,h) in yuzler:
       cv2.rectangle(kare,(x,y),(x+w,y+h),(0,0,255),2)
     cv2.imshow("Video", kare)
     if cv2.waitKey(25) & 0xFF==ord('q'):
---
+++ yuzler.py
@@ -0,0 +1,47 @@
+import os
+from PIL import Image
+import numpy as np
+import cv2
+
+yuz = cv2.CascadeClassifier('haarcascade-frontalface-default.xml ')
+#temel klasör dosyalarımızın temelde nerede olduğunun bilgisini verir
+temel_klasor = os.path.dirname(os.path.abspath(__file__))
+resim_klasor=os.path.join(temel_klasor,"resimler")
+#resim klasör resimlerimizin nerede olduğunun bilgisini döndürür
+y_labels=[]
+x_train=[]
+current_id=0;
+label_ids={}
+
+for kok_dizin,dizin,dosyalar in os.walk(resim_klasor):
+ for dosya in dosyalar:
+     if dosya.endswith("png") or dosya.endswith("jpg"):
+
+         path = os.path.join(kok_dizin,dosya)
+         label=os.path.basename(os.path.dirname(path).replace(" ","-")).lower()
+         #print(label,path)
+         if not label in label_ids:
+             label_ids[label]=current_id
+             current_id+=1
+             print(label_ids)
+
+         id=label_ids[label]
+
+         pil_resim=Image.open(path).convert("L")
+         resim_array = np.array(pil_resim, "uint8")
+         yuzler = yuz.detectMultiScale(resim_array, 1.3, 4)
+         for(x,y,w,h) in yuzler:
+             roi=resim_array[y:y+h,x:x+w]
+             x_train.append(roi)
+             y_labels.append(id)
+
+
+
+
+
+
+
+
+
+
+
