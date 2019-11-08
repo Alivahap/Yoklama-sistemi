5fa743d7f89f4b97a7de7f24521f7c0a6371b669
Ali Vahap Aydın <ali_vahap14@hotmail.com>
2019-11-08 03:39:00 +0300

    Gülme işlemini algılaması için dosya ekledim ve bazı düzeltmeler yaptım

--- Yuz_algilama.py
+++ Yuz_algilama.py
@@ -3,13 +3,17 @@ import numpy as np
 
 kmr = cv2.VideoCapture(0)
 
-while (kmr.isOpened()):
+while True:
     ret,kare = kmr.read()
+    yuz = cv2.CascadeClassifier('haarcascade-frontalface-default.xml ')
+   # gulme=cv2.CascadeClassifier('haarcascade_smile.xml ')
 
-    yuz = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    gri = cv2.cvtColor(kare,cv2.COLOR_BGR2GRAY)
-    yuzler = yuz.detectMultiScale(gri,1.1,4)
-    for(x,y,w,h) in yuzler:
+    gri_renk = cv2.cvtColor(kare,cv2.COLOR_BGR2GRAY)
+  #  gri_renk_gulme=cv2.cvtColor(kare,cv2.COLOR_BGR2GRAY)
+
+    faces = yuz.detectMultiScale(gri_renk,1.3,4)
+
+    for(x,y,w,h) in faces:
       cv2.rectangle(kare,(x,y),(x+w,y+h),(0,0,255),2)
     cv2.imshow("Video", kare)
     if cv2.waitKey(25) & 0xFF==ord('q'):
